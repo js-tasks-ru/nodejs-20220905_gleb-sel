ea840a58b226636d63c9b55efc99a83bbb15189d
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2022-10-16 21:06:25 +0300

    solve 01-shema mongoose

--- 06-mongodb-mongoose/01-schema-model/models/Product.js
+++ 06-mongodb-mongoose/01-schema-model/models/Product.js
@@ -1,7 +1,29 @@
 const mongoose = require('mongoose');
 const connection = require('../libs/connection');
+// const Category = require('./Category');
 
 const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: mongoose.ObjectId,
+    required: true,
+  },
+  subcategory: {
+    type: mongoose.ObjectId,
+    required: true,
+  },
+  images: [String],
 });
 
 module.exports = connection.model('Product', productSchema);
