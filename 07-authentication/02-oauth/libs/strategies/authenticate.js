42e09d1f4021d8b73226b29f25cf753d7824d451
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2022-10-23 16:07:19 +0300

    solve 07-02 auth

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,21 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (!email) return done(null, false, 'Не указан email');
+
+  await User.findOne({email: email}, async (err, user) => {
+    if (err) return done(err);
+    if (user) {
+      done(null, user);
+    } else {
+      try {
+        const userNew = await User.create({email: email, displayName: displayName});
+        done(null, userNew);
+      } catch (err) {
+        if (err.name !== 'ValidationError') throw err;
+        done(err, false, err.errors.email.message);
+      }
+    }
+  });
 };
